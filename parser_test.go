78c5c8c8c28897893a057e628beb744023461cf2
leiysky <leiysky@outlook.com>
2019-07-26 16:01:53 +0800

    Add testVisitor

--- parser_test.go
+++ parser_test.go
@@ -1,11 +1,31 @@
 package parser
 
-import "testing"
+import (
+	"fmt"
+	"testing"
+
+	"github.com/leiysky/parser/ast"
+)
+
+type testVisitor struct {
+	ast.Visitor
+}
+
+func (v *testVisitor) Enter(node ast.Node) (ast.Node, bool) {
+	fmt.Printf("%T\n", node)
+	return node, false
+}
+
+func (v *testVisitor) Leave(node ast.Node) (ast.Node, bool) {
+	return node, true
+}
 
 func TestParser(t *testing.T) {
 	parser := New()
-	parser.Parse(`
-MATCH (n)
+	stmt := parser.Parse(`
+MATCH (n)-[:Label *0123]-()
 WHERE n.id = 1
+SET n = {}
 RETURN n`)
+	stmt.Accept(&testVisitor{})
 }
