ade3d69f8914691289227193dc7dec4b4457ff58
leiysky <leiysky@outlook.com>
2019-08-09 21:17:30 +0800

    add comment

--- parser.go
+++ parser.go
@@ -6,14 +6,17 @@ import (
 	ps "github.com/leiysky/parser/parser"
 )
 
+// New will create a Parser
 func New() *Parser {
 	return &Parser{}
 }
 
+// Parser is used for parsing cypher
 type Parser struct {
 	l *ps.CypherLexer
 }
 
+// Parse will parse the cypher into ast.Stmt
 func (p *Parser) Parse(cypher string) ast.Stmt {
 	p.l = ps.NewCypherLexer(antlr.NewInputStream(cypher))
 	tokenStream := antlr.NewCommonTokenStream(p.l, antlr.LexerDefaultTokenChannel)
