2c4c5a9386dad7b7e32f14440170af8656c71000
leiysky <leiysky@outlook.com>
2019-08-09 21:17:39 +0800

    add comment

--- ast/ast.go
+++ ast/ast.go
@@ -1,23 +1,32 @@
 package ast
 
+// Node is basic type of ast.
 type Node interface {
 	Accept(visitor Visitor) (node Node, ok bool)
 	Text() string
 	SetText(text string)
+	Restore(ctx *RestoreContext)
 }
 
+// Stmt represents statement node.
 type Stmt interface {
 	Node
 	statementNode()
 }
 
+// Expr represents expression node.
 type Expr interface {
 	Node
 	exprNode()
 }
 
+// Visitor can visit ast.
 type Visitor interface {
+	// Enter would be called at the begin of Accept.
+	// The visited node will be replaced by returned Node.
+	// If skipChildren is true, the child nodes' Accept wouldn't be called.
 	Enter(n Node) (node Node, skipChildren bool)
 
+	// Accept will directly return Leave.
 	Leave(n Node) (node Node, ok bool)
 }
